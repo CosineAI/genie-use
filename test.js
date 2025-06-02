4da483e4d7e8ac5485e5b532e5fc7a0c5824a7af
Cameron Nimmo <cameronnimmo@hotmail.co.uk>
2025-06-02 17:10:05 +0100

    feat: add helloWorld function to print greeting
    Co-authored-by: Genie <genie@cosine.sh>

---
+++ test.js
@@ -0,0 +1,9 @@
+/**
+ * Prints "Hello, World!" to the console.
+ */
+function helloWorld() {
+    console.log("Hello, World!");
+}
+
+// Uncomment the following line to test the function
+// helloWorld();
